e60bb2a74a5541a5c2f0f272ef07584613012f75
9oelM <hj923@hotmail.com>
2018-08-04 11:39:32 +0000

    rename

--- eslintrc.js
+++
@@ -1,8 +0,0 @@
-/* 
-use airbnb's eslint rules
-https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb
-*/
-
-let eslintConfigAirbnb = require("eslint-config-airbnb");
-
-module.exports = eslintConfigAirbnb;
